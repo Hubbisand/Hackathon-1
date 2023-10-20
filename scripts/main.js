2a54a711e6ba5800f67c475b27af579820e51687
Hubbisand <Benjamin.Reid.Jackson@gmail.com>
2023-10-19 21:46:53 -0400

    Plays Music

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const overture = new Audio("audio/01 Overture.mp3");
+    overture.play();
+})
+const overture = new Audio("audio/01 Overture.mp3");
+overture.play();
